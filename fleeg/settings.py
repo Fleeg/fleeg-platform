0e558a90cabe6c18a40fdfa0b2f0ca99265a8731
Lucas Boscaini <lucasboscaini@gmail.com>
2017-07-10 21:42:09 -0300

    Move template and static to common app.

--- fleeg/settings.py
+++ fleeg/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@kih_p6c%p4f5)wz5z4y=pn$t5e=$7!89g6bg(r8-*s#er((bw'
 
 # SECURITY WARNING: don't run with debug or testing turned on in production!
-DEBUG = True #'debug' in sys.argv
-TESTING = True #'test' in sys.argv
+DEBUG = True    # 'debug' in sys.argv
+TESTING = True  # 'test' in sys.argv
 
 ALLOWED_HOSTS = []
 
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'common',
     'account',
     'link',
 ]
@@ -47,7 +48,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'common/templates'),
         ]
         ,
         'APP_DIRS': True,
@@ -114,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'common/static'),
 ]
 
 if TESTING:
