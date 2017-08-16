878901358221fdb3960b7c6500a4a254e81d3733
Lucas Boscaini <lucasboscaini@gmail.com>
2017-08-16 18:50:14 -0300

    Fix coverage task to find missed lines.
    * Once django-nose has a open issue about miss def and import lines when use config in django settings was necessary move that to use coverage directly.
    * Remove unnecessary configs in settings and add it in coveragerc file.
    * Change ci file to use the new coverage process.

--- fleeg/settings.py
+++ fleeg/settings.py
@@ -124,11 +124,3 @@ if TESTING:
     
     # Use nose to run all tests
     TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-    # Tell nose to measure coverage on the 'foo' and 'bar' apps
-    NOSE_ARGS = [
-        '--with-coverage',
-        '--cover-package=common,account,link',
-        '--cover-inclusive',
-        '--cover-html',
-    ]
