60d79b03fbb6c1ad70b16d323fe7fa4a77cb0abe
Lucas Boscaini <lucasboscaini@gmail.com>
2017-12-08 17:58:41 -0200

    Add test to list of notification page.

--- notification/tests.py
+++ notification/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from account.factories import AccountFactory, DEFAULT_PASSWORD
+
+
+class TestNotification(TestCase):
+    def setUp(self):
+        account = AccountFactory.create()
+        self.user = account.user
+
+    def test_access_notification_list(self):
+        self.client.login(username=self.user.username, password=DEFAULT_PASSWORD)
+        response = self.client.get(reverse('notifications'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'notification/list.html')
