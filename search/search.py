0d9b50730b799154988f27b4ae5c72dcc6aae323
Lucas Boscaini <lucasboscaini@gmail.com>
2018-01-08 18:43:01 -0200

    Basic implementation of search engine.
     * Create basic search for links and user
     * Implement advanced search to specify field to search
     * Add a new screen with a list of results

--- search/search.py
+++ search/search.py
@@ -1,9 +1,13 @@
+from django.db.models import Q
+
+from account.models import Account
+from link.models import Post
+
 
 class Search:
-    def __init__(self, query, executor):
+    def __init__(self, query):
         self.adv_separator = ':'
         self.results = []
-        self.executor = executor
         self.query_value = query
         self.words = query.lower().split(' ')
 
@@ -11,7 +15,8 @@ class Search:
         if self.is_advanced():
             self.advanced()
         else:
-            print('It is not supported yet.')
+            self.query_links()
+            self.query_users()
 
     def is_advanced(self):
         return self.adv_separator in self.words[0]
@@ -28,15 +33,45 @@ class Search:
         elif attr == 'tag':
             self.query_links_by_title()
         else:
-            print('attr not supported! try; user, title or tag')
+            # TODO: Throws a exception
+            print('attr not supported! try: user, title or tag')
 
     def query_users(self):
-        # username or first and last name
-        print('run user')
-        pass
+        accounts = Account.objects.filter(Q(user__username__in=self.words) |
+                                          Q(user__first_name__in=self.words) |
+                                          Q(user__last_name__in=self.words))
+        self.results += list(map(lambda i: self.result_type(i, 'user'), accounts))
 
     def query_links_by_title(self):
-        pass
+        q_filters = Q()
+        for word in self.words:
+            q_filters.add(Q(title__icontains=word), Q.OR)
+
+        posts = Post.objects.filter(q_filters)
+        self.results = list(map(lambda i: self.result_type(i, 'post'), posts))
 
     def query_links_by_tag(self):
-        pass
+        q_filters = Q()
+        for word in self.words:
+            q_filters.add(Q(tags__icontains=word), Q.OR)
+
+        posts = Post.objects.filter(q_filters)
+        self.results = list(map(lambda i: self.result_type(i, 'post'), posts))
+
+    def query_links(self):
+        q_filters = Q()
+        for word in self.words:
+            q_filters.add(Q(title__icontains=word), Q.OR)
+            q_filters.add(Q(url__icontains=word), Q.OR)
+            q_filters.add(Q(summary__icontains=word), Q.OR)
+            q_filters.add(Q(text__icontains=word), Q.OR)
+            q_filters.add(Q(tags__icontains=word), Q.OR)
+
+        # TODO: Add a for to add rank value based level search
+        posts = Post.objects.filter(q_filters)
+        self.results += list(map(lambda i: self.result_type(i, 'post'), posts))
+
+    @staticmethod
+    def result_type(item, r_type):
+        item.result_type = r_type
+        return item
--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from notification.models import Notification
 from search.search import Search
 
 
@@ -9,9 +10,9 @@ class SearchView:
     @staticmethod
     @login_required
     def list(request):
-        owner = request.user.accounts.first()
         query = request.GET.get('q', None)
-        search = Search(query, owner)
+        search = Search(query)
         search.process()
-        return render(request, 'search/list.html', {'search': search})
+        notify_count = Notification.objects.filter(owner__user=request.user, viewed=False).count()
+        return render(request, 'search/list.html', {'search': search, 'notify_count': notify_count})
 
