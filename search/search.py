6a66172e7e1378937064d24365523ec05a13e034
Lucas Boscaini <lucasboscaini@gmail.com>
2018-01-25 17:34:23 -0200

    Add support to level sort in search
     * Add function to filter by level.
     * Concatenate results by level sorted by desc created_at.
     * Add capability standards queries and advanced.
     * Add capability to query using filter/ordering by owner follow networking or ignoring it.

--- search/search.py
+++ search/search.py
@@ -16,8 +16,7 @@ class Search:
         if self.is_advanced():
             self.advanced()
         else:
-            self.query_links()
-            self.query_users()
+            self.query_level(self.filter_links(), self.filter_users())
 
     def is_advanced(self):
         return self.adv_separator in self.words[0]
@@ -28,37 +27,20 @@ class Search:
             self.words = [word] + self.words
 
         if attr == 'user':
-            self.query_users()
+            self.query_level(q_filter_user=self.filter_users())
         elif attr == 'title':
-            self.query_links_by_title()
+            self.query_level(q_filter_link=self.filter_links_by_title())
         elif attr == 'tag':
-            self.query_links_by_tag()
+            self.query_level(q_filter_link=self.filter_links_by_tag())
         else:
             raise SearchException('Attr not supported! try: user, title or tag')
 
-    def query_users(self):
-        accounts = Account.objects.filter(Q(user__username__in=self.words) |
-                                          Q(user__first_name__in=self.words) |
-                                          Q(user__last_name__in=self.words))
-        self.results += list(map(lambda i: self.result_type(i, 'user'), accounts))
+    def filter_users(self):
+        return Q(Q(user__username__in=self.words) |
+                 Q(user__first_name__in=self.words) |
+                 Q(user__last_name__in=self.words))
 
-    def query_links_by_title(self):
-        q_filters = Q()
-        for word in self.words:
-            q_filters.add(Q(title__icontains=word), Q.OR)
-
-        posts = Post.objects.filter(q_filters)
-        self.results = list(map(lambda i: self.result_type(i, 'post'), posts))
-
-    def query_links_by_tag(self):
-        q_filters = Q()
-        for word in self.words:
-            q_filters.add(Q(tags__icontains=word), Q.OR)
-
-        posts = Post.objects.filter(q_filters)
-        self.results = list(map(lambda i: self.result_type(i, 'post'), posts))
-
-    def query_links(self):
+    def filter_links(self):
         q_filters = Q()
         for word in self.words:
             q_filters.add(Q(title__icontains=word), Q.OR)
@@ -66,30 +48,55 @@ class Search:
             q_filters.add(Q(summary__icontains=word), Q.OR)
             q_filters.add(Q(text__icontains=word), Q.OR)
             q_filters.add(Q(tags__icontains=word), Q.OR)
+        return q_filters
 
-        # TODO: Add a for to add rank value based level search
-        posts = Post.objects.filter(q_filters)
-        self.results += list(map(lambda i: self.result_type(i, 'post'), posts))
+    def filter_links_by_title(self):
+        q_filters = Q()
+        for word in self.words:
+            q_filters.add(Q(title__icontains=word), Q.OR)
+        return q_filters
 
-    def link_sort_by_level(self):
-        my = Q(owner=self.owner)
-        following = Q(owner__followers__owner=self.owner)
-        following_follow = Q(owner_followers__owner__followers__owner=self.owner)
-        not_follow = Q(my | following | following_follow)
+    def filter_links_by_tag(self):
+        q_filters = Q()
+        for word in self.words:
+            q_filters.add(Q(tags__icontains=word), Q.OR)
+        return q_filters
 
-    def user_sort_by_level(self):
-        my = Q(id=self.owner.id)
-        following = Q(followers__owner=self.owner)
-        following__follow = Q(followers__owner__followers__owner=self.owner)
-        not_follow = Q(my | following | following__follow)
+    def query_level(self, q_filter_link=None, q_filter_user=None):
+        levels = [
+            {'link': Q(owner=self.owner), 'user': Q(id=self.owner.id)},
+            {'link': Q(owner__followers__owner=self.owner), 'user': Q(followers__owner=self.owner)},
+            {'link': Q(owner_followers__owner__followers__owner=self.owner),
+             'user': Q(followers__owner__followers__owner=self.owner)},
+        ]
+        last_level = {'link': ~Q(levels[0]['link'] | levels[1]['link'] | levels[2]['link']),
+                      'user': ~Q(levels[0]['user'] | levels[1]['user'] | levels[2]['user'])}
+        if self.owner:
+            for level in levels:
+                self.results += self.process_query(q_filter_link, q_filter_user, level)
+            self.results += self.process_query(q_filter_link, q_filter_user, last_level)
+        else:
+            self.results = self.process_query(q_filter_link, q_filter_user)
 
-    def process_link_query(self, q_level_filter, q_filter):
-        return Post.objects.filter(q_level_filter).filter(q_filter).annotate(
-            result_type='post').order_by('-created_at')
+    @staticmethod
+    def process_query(q_filter_link=None, q_filter_user=None, level=None):
+        level_results = []
 
-    def process_user_query(self, q_level_filter, q_filter):
-        return Account.objects.filter(q_level_filter).filter(q_filter).annotate(
-            result_type='user').order_by('-created_at')
+        if level is None:
+            level = {'link': Q(), 'user': Q()}
+
+        if q_filter_link:
+            q_filter_link.add(level['link'])
+            level_results += Post.objects.filter(q_filter_link).annotate(
+                result_type='post').order_by('-created_at')
+        if q_filter_user:
+            q_filter_user.add(level['user'])
+            level_results += Account.objects.filter(q_filter_user).annotate(
+                result_type='user').order_by('-created_at')
+
+        if q_filter_link and q_filter_user:
+            level_results = sorted(level_results, key=lambda item: item['created_at'], reverse=True)
+        return level_results
 
 
 class SearchException(Exception):
